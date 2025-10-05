7bb3ed5de19c83dc3d247a627f792aef573c5ecf
asoderlind <axesode@gmail.com>
2025-10-05 14:06:10 +0200

    add host

--- flake.nix
+++ flake.nix
@@ -9,31 +9,41 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations = {
-      "axel@mac-home" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."aarch64-darwin"; # or "x86_64-darwin" for Intel
-        modules = [
-          ./home.nix
-          ./hosts/mac-home.nix
-        ];
-      };
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    {
+      homeConfigurations = {
+        "axel@mac-home" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."aarch64-darwin"; # or "x86_64-darwin" for Intel
+          modules = [
+            ./home.nix
+            ./hosts/mac-home.nix
+          ];
+        };
 
-      "axel-soderlind@ubuntu-work" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [
-          ./home.nix
-          ./hosts/ubuntu-work.nix
-        ];
-      };
+        "axel-soderlind@ubuntu-work" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          modules = [
+            ./home.nix
+            ./hosts/ubuntu-work.nix
+          ];
+        };
 
-      "axel-soderlind@nixvm-work" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        modules = [
-          ./home.nix
-          ./hosts/nixvm-work.nix
-        ];
+        "axel-soderlind@nixvm-work" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          modules = [
+            ./home.nix
+            ./hosts/nixvm-work.nix
+          ];
+        };
+
+        "axel@nixos-home" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          modules = [
+            ./home.nix
+            ./hosts/nixos-home.nix
+          ];
+        };
       };
     };
-  };
 }
